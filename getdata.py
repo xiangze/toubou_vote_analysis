4497eef312e168ac3316954ac763ee67b6ea4579
xiangze <xiangze@gmail.com>
2022-11-14 08:46:54 +0900

    initial commit(not run)

---
+++ getdata.py
@@ -0,0 +1,18 @@
+import bs4
+import os
+import requests
+
+N=18
+ddir="data"
+url=""
+
+if(os.path.exists(ddir) and os.path.isdir(ddir)):
+    ddir=""
+else:
+    requests.
+
+for n in range(1,N):
+    with open(ddir/str(n)) as f:
+        page=f.read()
+
+        =page
